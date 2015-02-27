5f1d00eb6e49d7ab4ea6dd797a74a0f8b575a078
Danny Willems <dan.wil@hotmail.com>
2015-02-27 20:41:10 +0100

    interpolation.mli : align parameters in declaration

--- interpolation.mli
+++ interpolation.mli
@@ -7,8 +7,10 @@ val least_squares : (float * float) array -> float array -> float array
 (*
  * Chebyshev - Gausse - Lobatto node between a and b with n nodes
 *)
-val cgl_node : ?i:int -> float -> float -> int -> (float -> float) -> (float * float) list
+val cgl_node :  ?i:int -> float -> float -> int -> (float -> float)
+                -> (float * float) list
 
-val lagrange_evaluation_k : ?i:int -> (float * float) array -> float -> float -> float
+val lagrange_evaluation_k : ?i:int -> (float * float) array -> float -> float
+                            -> float
 
 val lagrange_evaluation : ?i:int -> (float * float) array -> float -> float
