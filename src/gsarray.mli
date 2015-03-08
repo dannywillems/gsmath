a73dd340fef8fd11acc40b691a263581a9155f95
Danny Willems <dan.wil@hotmail.com>
2015-03-08 11:44:16 +0100

    gsarray interface : change comments

--- src/gsarray.mli
+++ src/gsarray.mli
@@ -7,13 +7,13 @@
 (** Apply a function (first param) on each element of the array *)
 val iter : ('a -> 'a) -> 'a array -> unit
 
-(* Copy data from the second array into the first array *)
+(** Copy data from the second array into the first array *)
 val copy_data : 'a array -> 'a array -> unit
 
-(* Print a matrix of float with 4 spaces between each value and a \n at the end
+(** Print a matrix of float with 4 spaces between each value and a \n at the end
  * of a line
-*)
+ *)
 val print_matrix_float : float array array -> unit
 
-(* Print an array of float with 4 spaces between each value *)
+(** Print an array of float with 4 spaces between each value *)
 val print_array_float : float array -> unit
