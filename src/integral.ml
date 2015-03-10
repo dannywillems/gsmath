4802b3fc9428f1f83206a9d311760be15f5201a3
Danny Willems <dan.wil@hotmail.com>
2015-03-10 20:36:31 +0100

    integral : trapeze method with points

--- src/integral.ml
+++ src/integral.ml
@@ -1,3 +1,13 @@
+let interval a b n =
+    let step = (b -. a) /. (float n) in
+    let c = Array.create n 0. in
+    c.(0) <- a;
+    for i = 1 to (n - 1)
+    do
+        c.(i) <- a +. (float i) *. step;
+    done;
+    c;;
+
 let rectangle_local f a b =
     ((f b) -. (f a)) /. (b -. a);;
 
@@ -9,10 +19,9 @@ let rec rectangle f a b ?(i = 0) n =
         let a_n1 = a +. (b -. a) /. (float n) *. (float (i + 1)) in
         (rectangle_local f a_n0 a_n1) +. (rectangle f a b ~i:(i+1) n);;
 
-(* Trapeze method implementation for integrate calculus *)
-
+(** Trapeze method implementation for integrate calculus *)
 let trapeze_local f a b =
-    ((f a) +. (f b)) /. 2. *. (b -. a);;
+    ((f a) +. (f b)) *. 0.5 *. (b -. a);;
 
 let rec trapeze f a b ?(i = 0) n =
     if i = n then
@@ -21,3 +30,13 @@ let rec trapeze f a b ?(i = 0) n =
         let a_n0 = a +. (b -. a) /. (float n) *. (float i) in
         let a_n1 = a +. (b -. a) /. (float n) *. (float (i + 1)) in
         (trapeze_local f a_n0 a_n1) +. (trapeze f a b ~i:(i+1) n);;
+
+let rec trapeze_point x y ci =
+    let n = Array.length x in
+    let a = Array.create n 0. in
+    a.(0) <- ci;
+    for i = 0 to n - 1
+    do
+        a.(i) <- a.(i) +. (y.(i) -. a.(0)) *. 0.5 *. (x.(i) -. x.(0));
+    done;
+    a;;
--- src/integral.mli
+++ src/integral.mli
@@ -5,6 +5,10 @@
  @author Danny Willems
 *)
 
+val interval: float -> float -> int -> float array
+
 val rectangle: (float -> float) -> float -> float -> ?i:int -> int -> float
 
 val trapeze: (float -> float) -> float -> float -> ?i:int -> int -> float
+
+val trapeze_point: float array -> float array -> float -> float array
