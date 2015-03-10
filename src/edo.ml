ff0a5b2d1c58facd76c599bcd6da548dcfd21e9e
Danny Willems <dan.wil@hotmail.com>
2015-03-10 20:47:12 +0100

    edo : Fix bug when the initial condition was changed during computation

--- src/edo.ml
+++ src/edo.ml
@@ -4,9 +4,11 @@ open Vector.Infix
 (* Euler *)
 let euler f a b y0 n =
     let h = (b -. a) /. (float n) in
-    let y = Array.create n y0 in
+    let y = Vector.create_array_3d n in
+    Vector.copy y.(0) y0;
     for i = 0 to (n - 2) do
-        y.(i + 1) <- y.(i) ++. h **. (f (a +. h *. (float i)) (y.(i)));
+        Vector.copy y.(i + 1)   (y.(i) ++. h **.
+                                (f (a +. h *. (float i)) (y.(i))))
     done;
     y;;
 
@@ -16,7 +18,7 @@ let euler f a b y0 n =
 (* TODO *)
 (* How to implement implicit methods ? *)
 
-let middle_point f a b y0 n = Array.create n y0;;
+let middle_point f a b y0 n = Vector.create_array_3d n;;
 
 (******************************************************************************)
 (* Runge kutta order 4 *)
@@ -69,22 +71,25 @@ let rk4_coef_c n i =
 (* ------------------- *)
 
 let intermediate_point f t y0 h coef =
-    let i_point = Array.create 4 y0 in
+    let i_point = Vector.create_array_3d 4 in
+    Vector.copy i_point.(0) y0;
     for i = 1 to 3 do
         for j = 0 to (i - 1) do
             let aij = rk4_coef_a coef i j in
             let cj = rk4_coef_c coef j in
-            i_point.(i) <-  i_point.(i) ++. (aij *. h) **.
-                            (f (t +. cj *. h) i_point.(j));
+            Vector.copy i_point.(i) (i_point.(i) ++. (aij *. h) **.
+                                    (f (t +. cj *. h) i_point.(j)))
         done;
     done;
     i_point;;
 
 let rk4 f a b y0 n =
     let h = (b -. a) /. (float n) in
-    let y = Array.create n y0 in
+    let y = Vector.create_array_3d n in
     let coef = rk4_coef in
-    let points = Array.create 4 y0 in
+    let points = Vector.create_array_3d 4 in
+
+    Vector.copy y.(0) y0;
     for i = 1 to (n - 1) do
         let ti = a +. h *. (float (i - 1)) in
         Vector.copy_array   points
@@ -95,12 +100,12 @@ let rk4 f a b y0 n =
                                 h (* h *)
                                 coef (* n *)
                             );
-        y.(i) <- y.(i - 1);
+        Vector.copy y.(i) y.(i - 1);
         for j = 0 to 3 do
             let bj = rk4_coef_b coef j in
             let cj = rk4_coef_c coef j in
-            y.(i) <-    y.(i) ++.
-                        (bj *. h) **. (f (ti +. h *. cj) points.(j))
+            Vector.copy y.(i)   (y.(i) ++. (bj *. h) **.
+                                (f (ti +. h *. cj) points.(j)))
         done;
     done;
     y;;
