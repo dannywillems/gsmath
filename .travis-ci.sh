9def709b437ea2be7119270b81bf044206910eb6
Danny Willems <contact@danny-willems.be>
2016-04-23 18:43:24 +0200

    add travis

---
+++ .travis-ci.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: .travis-ci.sh
+#
+#         USAGE: ./.travis-ci.sh
+#
+#   DESCRIPTION: build gsmath.cma
+#
+#        AUTHOR: Danny Willems <contact@danny-willems.be>
+#       CREATED: 04/06/2016 11:26
+#      REVISION: 1.0
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+export OPAMYES=1
+
+if [ -f "$HOME/.opam/config" ]; then
+    opam update
+    opam upgrade
+else
+    opam init
+fi
+
+if [ -n "${OPAM_SWITCH}" ]; then
+    opam switch ${OPAM_SWITCH}
+fi
+eval `opam config env`
+
+export OCAMLRUNPARAM=b
+
+# Test for make and make clean
+make build
